3576c7328e55526581a1a8d2875e70bfcacd6b92
PaoloIncollu <paoloinco@gmail.com>
2024-08-27 12:07:34 +0200

    snack completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,24 @@
+/*
+    Dare la possibilità di inserire due parole.
+    Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+    Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+*/
 
-console.log('java collegato')
+const first = prompt('Inserisci prima parola');
+const second = prompt('Inserisci seconda parola');
+
+if(first.length == second.length)
+{
+
+    console.log ('Le parole inserite hanno la stessa lunghezza ','Prima parola:' + first , 'Seconda parola:' + second);
+}
+
+else if (first.length > second.length){
+
+    console.log('La prima parola è più lunga :' + first);
+}
+
+else {
+
+    console.log('La seconda parola è più lunga:' + second);
+}
